900ace7032e63308804e10029bc8840072faa1e4
Mahyar Najibi <mahyarnajibi@gmail.com>
2016-01-29 16:06:22 -0800

    Adding some new parameters for training

--- config.lua
+++ config.lua
@@ -11,8 +11,10 @@ function config.parse(arg)
 	cmd:option('-use_difficult_objs', true, 'Whether to load the difficult examples or not')
 	cmd:option('-scale', 600, 'Scale used for training and testing, currently single scale!')
 	cmd:option('-max_size', 1000, 'Max pixel size of the longest side of a scaled input image')
-	cmd:option('-img_per_batch', 2, 'Images to use per minibatch')
-	cmd:option('-n_threads',4, 'Number of threads used for training')
+	cmd:option('-img_per_batch', 2, 'Images to be used per minibatch')
+	cmd:option('-GPU_ID',3,'Main GPU Id to be used')
+	cmd:option('-n_threads',2, 'Number of threads used for training')
+	cmd:option('-nGPU',1,'Number of GPUs to be used for training')
 	cmd:option('-roi_per_img', 64, 'Minibatch size')
 	cmd:option('-fg_fraction', 0.25, 'Fraction of minibatch that is labeled foreground (i.e. class > 0)')
 	cmd:option('-fg_threshold', 0.5, 'Overlap threshold for a ROI to be considered foreground (if >= FG_THRESH)')
@@ -20,7 +22,6 @@ function config.parse(arg)
 	cmd:option('-bg_threshold_lo',0.1, 'Low overlap threshold for a ROI to be considered background')
 	cmd:option('-use_flipped', true, 'Use horizontally-flipped images during training?')
 	cmd:option('-bbox_threshold', 0.5, 'Overlap required between a ROI and ground-truth box in order for that ROI to be used as a bounding-box regression training example')
-	cmd:option('-snapshot_iters', 10000, 'Iterations between snapshots')
 	cmd:option('-nms', 0.3, 'Overlap threshold used for non-maximum suppression (suppress boxes with IoU >= this val)')
 	cmd:option('-pixel_means', {102.9801,115.9465,122.7717}, 'Pixel mean values (BGR order)')
 	cmd:option('-eps', 1e-14, 'Epsilon')
@@ -30,10 +31,18 @@ function config.parse(arg)
 	cmd:option('-test_img_set','test','Image set to be used for testing')
 	cmd:option('-train_img_set','trainval','Image set to be used for test')
 	cmd:option('-cache','./cache','Directory used for saving cache data')
+	cmd:option('-optim_momentum',0.9,'Momentum used during optimization')
+	cmd:option('-optim_regimes',{{30000,1e-3,5e-4},{10000,1e-4,5e-4}},'Optimization regime, each row is number of iterations, learning rate, and weight decay')
+	--cmd:option('-optim_regimes',{{100,1e-3,5e-4},{100,1e-4,5e-4}},'Optimization regime, each row is number of iterations, learning rate, and weight decay')
+	cmd:option('-optim_snapshot_iters', 10000, 'Iterations between snapshots')
+	--cmd:option('-optim_snapshot_iters', 50, 'Iterations between snapshots')
+	cmd:option('-save_path','./data/trained_models','Path to be used for saving the trained models')
+	cmd:option('-log_path','./data/log')
 	-- Parsing the command line 
 	config = cmd:parse(arg or {})
-
 	return config
 end
+
+
 
 return config
