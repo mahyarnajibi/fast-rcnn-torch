d63dcb7b174c706be2f712c28f0ffa5a780f506e
Mahyar Najibi <mahyarnajibi@gmail.com>
2016-01-29 16:46:08 -0800

    Added normalization to the layer

--- train/WeightedSmoothL1Criterion.lua
+++ train/WeightedSmoothL1Criterion.lua
@@ -3,17 +3,19 @@ local WeightedSmoothL1Criterion, parent = torch.class('detection.WeightedSmoothL
 function WeightedSmoothL1Criterion:__init(sizeAverage)
    parent.__init(self)
    -- Local SmoothL1Loss
-   self._smoothL1Loss = nn.SmoothL1Criterion(sizeAverage)
+   self._smoothL1Loss = nn.SmoothL1Criterion()
+   self._smoothL1Loss.sizeAverage = false
 end
 
 function WeightedSmoothL1Criterion:updateOutput(input, target)
 	-- input contains the output of the network
 	-- target has the labels as the first element and loss weights as the second element
-	local repeat_size = input:size(2) / target[1]:size(2) 
+   local repeat_size = input:size(2) / target[1]:size(2) 
 	local targets = target[1]:repeatTensor(1,repeat_size)
 	local weights = target[2]
-
+   local n_row = targets:size(1)
 	self.output = self._smoothL1Loss:forward(input[weights],targets[weights])	
+   self.output = self.output / n_row
    return self.output
 end
 
@@ -21,7 +23,8 @@ function WeightedSmoothL1Criterion:updateGradInput(input, target)
    local repeat_size = input:size(2) / target[1]:size(2) 
    local targets = target[1]:repeatTensor(1,repeat_size)
    local weights = target[2]
-   local smoothL1grad = self._smoothL1Loss:backward(input[weights],targets[weights])
+   local n_row = targets:size(1)
+   local smoothL1grad = self._smoothL1Loss:backward(input[weights],targets[weights])/n_row
    self.gradInput:resizeAs(input):zero()
    self.gradInput[weights] = smoothL1grad
    return self.gradInput
