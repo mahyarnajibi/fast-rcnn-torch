87e5101139fef81c708b3ad4ed670a8cd31ffc72
Mahyar Najibi <mahyarnajibi@gmail.com>
2016-02-01 10:56:51 -0800

    Adding scripts for downloading files

---
+++ scripts/get_frcnn_model.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+dir_name=${PWD##*/}
+echo $dir_name
+
+file_name=frcnn_torch_models.tar.gz
+URL=http://www.umiacs.umd.edu/~najibi/data
+
+
+if [ "$dir_name" = "scripts" ]; then
+	save_path=../data/trained_models
+else
+	save_path=./data/trained_models
+fi
+
+
+file_path=${save_path%%/}/$file_name
+
+
+if [ -f "$file_path" ]; then
+	echo "File already exits!"
+else
+	if [ ! -d "$save_path" ]; then
+		mkdir $save_path
+	fi
+	echo "Downloading the fast-rcnn_models..."
+	wget $URL -O $file_path
+	echo "Uzip the file..."
+
+	tar zxvf $file_path -C $save_path
+	echo "Done!"
+fi
---
+++ scripts/get_proposals.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+dir_name=${PWD##*/}
+echo $dir_name
+
+file_name=selective_search_data.tgz
+URL=http://www.cs.berkeley.edu/~rbg/fast-rcnn-data/$file_name
+
+
+if [ "$dir_name" = "scripts" ]; then
+  save_path=../data/datasets/selective_search
+else
+  save_path=./data/datasets/selective_search
+fi
+
+
+file_path=${save_path%%/}/$file_name
+
+
+if [ -f "$file_path" ]; then
+  echo "File already exits!"
+else
+  if [ ! -d "$save_path" ]; then
+    mkdir $save_path
+  fi
+  echo "Downloading the fast-rcnn_models..."
+  wget $URL -O $file_path
+  echo "Uzip the file..."
+
+  tar zxvf $file_path -C $save_path
+  echo "Done!"
+fi
