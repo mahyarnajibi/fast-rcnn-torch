ccd0370c7089d685b874e036490509cfb29dc9e5
Mahyar Najibi <mahyarnajibi@gmail.com>
2016-02-12 10:48:48 -0800

    Updating main

--- main_train.lua
+++ main_train.lua
@@ -1,11 +1,12 @@
 -- Require the detection package
 require 'detection'
 
+
 -- Paths
 local dataset_name = config.dataset
 local image_set = config.train_img_set
 local dataset_dir = paths.concat(config.dataset_path,dataset_name)
-local ss_dir = './data/datasets/selective_search_data/'
+local ss_dir = '../frcnn_torch/data/datasets/selective_search/'
 local ss_file =  paths.concat(ss_dir, dataset_name .. '_' .. image_set .. '.mat')
 local param_path = config.pre_trained_file
 local model_path = config.model_def
@@ -13,10 +14,13 @@ local model_path = config.model_def
 
 -- Loading the dataset
 local dataset = detection.DataSetPascal({image_set = image_set, datadir = dataset_dir, roidbdir = ss_dir , roidbfile = ss_file})
+--local dataset = detection.DataSetCoco({image_set = image_set, datadir = dataset_dir})
 
 -- Creating the detection network
-network = detection.Net(model_path,param_path)
-
+model_opt = {}
+model_opt.fine_tunning = not config.resume_training
+-- model_opt.f
+network = detection.Net(model_path,param_path,model_opt)
 -- Creating the network wrapper
 local network_wrapper = detection.NetworkWrapper() -- This adds train and test functionality to the global network
 
